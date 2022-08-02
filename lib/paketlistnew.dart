dedf78a318033fefb00b0addd03364bc2745ed76
Andy Pangadiansjah <voxtmault@gmail.com>
2022-08-02 11:37:09 +0700

    Added Backend Compability

--- lib/paketlistnew.dart
+++ lib/paketlistnew.dart
@@ -1,11 +1,51 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
+import 'dart:convert';
+import 'package:firstproject/cobabelajarwidget.dart';
+import 'package:http/http.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/number_symbols_data.dart';
 
-class packetList extends StatelessWidget {
+class packetList extends StatefulWidget {
   const packetList({Key? key}) : super(key: key);
+
+  @override
+  State<packetList> createState() => _packetListState();
+}
+
+class _packetListState extends State<packetList> {
+  int index = 0;
+  var _numFormat = NumberFormat("#,##0", "en_US");
+  List<Packages> listPackages = [];
+
+  Future<Null> _fetchDataProduct() async {
+    String url = "http://10.5.50.22:38500/packages";
+
+    var loading = true;
+    final response = await get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      loading = true;
+      final data = jsonDecode(response.body);
+      print(data);
+      setState(() {
+        for (Map i in data['Data']) {
+          listPackages.add(Packages.fromJson(i));
+        }
+
+        loading = false;
+      });
+    } else {
+      debugPrint("Something went wrong");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDataProduct();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,29 +78,26 @@ class packetList extends StatelessWidget {
           SliverList(
               delegate: SliverChildBuilderDelegate(
             (BuildContext context, int index) {
+              final getData = listPackages[index];
               return Container(
                 margin: EdgeInsets.only(left: 20, right: 20, top: 16),
-                child: packetList_Packet(
-                  indexnya: index,
+                child: loadPackages(
+                  context,
+                  getData.packetName,
+                  getData.packetPrice,
+                  getData.packetDuration,
                 ),
               );
             },
-            childCount: 10,
+            childCount: listPackages.length,
           ))
         ],
       ),
     );
   }
-}
 
-class packetList_Packet extends StatelessWidget {
-  int indexnya;
-
-  packetList_Packet({Key? key, required this.indexnya}) : super(key: key);
-  final _numFormat = NumberFormat("#,##0", "en_US");
-  @override
-  Widget build(BuildContext context) {
-    indexnya = indexnya + 1;
+  Widget loadPackages(BuildContext context, String packetName,
+      String packetPrice, String packetDuration) {
     return Card(
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(20))),
@@ -73,9 +110,11 @@ class packetList_Packet extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                //Package Logo
                 Container(
                     height: MediaQuery.of(context).size.height * 0.1,
                     child: Image.asset("images/crosslogo.png")),
+                //Package Details
                 Container(
                   padding: EdgeInsets.only(top: 10, bottom: 10),
                   decoration:
@@ -85,19 +124,21 @@ class packetList_Packet extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
+                          //Packet Name
                           Container(
                               margin: EdgeInsets.only(left: 16),
                               child: Text(
-                                "Speed ${indexnya + 1} Mb",
+                                packetName,
                                 style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 20,
                                 ),
                               )),
+                          //Packet Price
                           Container(
                               margin: EdgeInsets.only(right: 28),
                               child: Text(
-                                "Rp." + _numFormat.format(15000),
+                                packetPrice,
                                 style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 15,
@@ -108,15 +149,17 @@ class packetList_Packet extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
+                          //Packet Duration
                           Container(
                               margin: EdgeInsets.only(left: 18, top: 6),
                               child: Text(
-                                "Valid for 30 Days",
+                                packetDuration,
                                 style: TextStyle(
                                   fontSize: 10,
                                   fontWeight: FontWeight.bold,
                                 ),
                               )),
+                          //Buy Button
                           Container(
                               margin: EdgeInsets.only(right: 18),
                               height: 25,
@@ -140,4 +183,47 @@ class packetList_Packet extends StatelessWidget {
               ],
             )));
   }
+}
+
+class Packages {
+  int packetId, totalDevices;
+  String packetName,
+      packetPrice,
+      packetType,
+      packetDownloadSpeed,
+      packetUploadSpeed,
+      packetDuration;
+
+  Packages({
+    required this.packetId,
+    required this.totalDevices,
+    required this.packetName,
+    required this.packetPrice,
+    required this.packetType,
+    required this.packetDownloadSpeed,
+    required this.packetUploadSpeed,
+    required this.packetDuration,
+  });
+
+  factory Packages.fromJson(Map<dynamic, dynamic> json) => Packages(
+        packetId: json["Id"],
+        packetName: json["Name"],
+        packetPrice: json["Price"],
+        packetType: json["Type"],
+        packetDownloadSpeed: json["SDownload"],
+        packetUploadSpeed: json["SUpload"],
+        totalDevices: json["TotalDevices"],
+        packetDuration: json["Duration"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": packetId,
+        "total_devices": totalDevices,
+        "name": packetName,
+        "price": packetPrice,
+        "type": packetPrice,
+        "s_download": packetDownloadSpeed,
+        "s_upload": packetUploadSpeed,
+        "duration_days": packetDuration,
+      };
 }
