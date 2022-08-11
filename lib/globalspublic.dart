661cee5b4e18b63cea08714502a0bc927b0972f5
dimas-renanda <100029762+dimas-renanda@users.noreply.github.com>
2022-08-11 19:14:25 +0700

    ganti pake phoenix

--- lib/globalspublic.dart
+++ lib/globalspublic.dart
@@ -1,7 +1,7 @@
 library my_prj.globals;
 
 //API URL
-String uriString = "http://10.5.50.22:38500";
+String uriString = "http://phoenix.crossnet.co.id:38600";
 
 bool isLoggedIn = false;
 int numpagenya = 0;
